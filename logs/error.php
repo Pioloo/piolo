27407a003d548f1a274c0dfc07252e0c8987a8e2
root <root@Lap.(none)>
2013-06-24 14:14:18 -0500

    Agregamos los demas archivos

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-06-24 15:05:30 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2013-06-24	15:05:30	INFO	127.0.0.1	Joomla FAILURE: 	Usuario no existe
